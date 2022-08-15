e6ea5c6d88b2a7b9529d397437c60a4e7b4c2103
Vlad Mansurov <m_v2709@mail.ru>
2022-08-15 16:39:55 +0300

    Изменил настройки на добавление несколько точек входа

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: {
+    main: './index.js',
+  },
   output: {
-    filename: 'js/main.js',
+    filename: 'js/[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
